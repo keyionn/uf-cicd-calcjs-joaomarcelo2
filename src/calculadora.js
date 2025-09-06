65b973bb991d06d178bfefcebaece62037a6b05b
João Marcelo <joaomarcelo201705@gmail.com>
2025-09-06 17:52:30 +0000

    quinto commit

--- src/calculadora.js
+++ src/calculadora.js
@@ -7,9 +7,29 @@ function subtract(a, b) {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+function divide(a, b) {
+        if (b === 0) {
+        throw new Error("Divisão por zero não é permitida.");
+        }
+        return a / b;
+    }
+
+
+
 // Exportar as funções
 
 module.exports = {
     add,
-    subtract
+    subtract,
+    divide,
 };
